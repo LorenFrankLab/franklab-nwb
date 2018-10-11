dbeeffd22d2ddc41e25c0ba7644bfddbf8f20d69
Tom Davidson <tjd@alum.mit.edu>
2018-10-11 12:10:01 -0700

    Refactor Matlab loading to capture structure/naming of FilterFramework .mat files
    We now create nested dicts with 1-indexed keys (same numbering as FilterFramework) for the base tree of nested cell arrays

--- nspike_helpers.py
+++ nspike_helpers.py
@@ -6,65 +6,87 @@ import numpy as np
 import os as os
 import re as re
 from datetime import datetime
+import warnings
 
 
 from struct import unpack
 from array import array
 
+def loadmat_ff(filename, varname):
+    '''
+    Parse Frank Lab FilterFramework .mat files (with nested row vector cell arrays
+    representing day/epoch/tetrode[/cluster].
+    '''
+    data = sio.loadmat(filename, struct_as_record=False, squeeze_me=False)
+    data = data[varname]
+    if isinstance(data, np.ndarray):
+        return _check_arr_ff(data, cellvec_to_dict=True)
+    else:
+        warnings.warn("Matlab variable is not an ndarray, returning as-is; type = %s" % type(elem))
+        return (data)
 
-def loadmat2(filename):
-        '''
-        this function is the same as above but needs to be used when getting rid of empty entries doesn't work
-        '''
-        data = sio.loadmat(filename, struct_as_record=False, squeeze_me=False)
-        return _check_keys(data)
+def _check_dict_ff(d):
+    '''
+    A recursive function which converts matobjects (e.g. structs) to dicts
+    '''
+    for k, v in d.items():
+        if isinstance(v, sio.matlab.mio5_params.mat_struct):
+            d[k] = _check_dict_ff(_struct_to_dict(v)) 
+        elif isinstance(v, np.ndarray):
+            d[k] = _check_arr_ff(v, cellvec_to_dict=False)
+    return d
 
-def _check_keys(data):
-        '''
-        checks if entries in dictionary are mat-objects. If yes
-        todict is called to change them to nested dictionaries
-        '''
-        for key in data:
-            if isinstance(data[key], sio.matlab.mio5_params.mat_struct):
-                data[key] = _todict(data[key])
-            elif isinstance(data[key],np.ndarray):
-                data[key] = _check_arr(data[key])
-        return data
+    
+def _check_arr_ff(arr, cellvec_to_dict):
+    '''
+    A recursive function which constructs dicts with 1-indexed keys from 
+    row-vector Matlab cellarrays (other-shaped cell arrays are left as ndarrays), 
+    recursing into the non-empty elements. We also squeeze singleton nodes if 
+    they are 'leaf' nodes.
+    '''
+    # If this is a numeric ndarray, then bail, we only want to handle 
+    # Matlab cell arrays, which are of dtype 'O'/np.object
+    if arr.dtype != np.object: return arr
 
-def _todict(matobj):
-        '''
-        A recursive function which constructs from matobjects nested dictionaries
-        '''
-        # reuse the dict that sio.loadmat created for us
-        d = matobj.__dict__
-        del d['_fieldnames'] # remove private housekeeping item from dict
-        
-        for k, v in d.items():
-                if isinstance(v, sio.matlab.mio5_params.mat_struct):
-                        d[k] = _todict(v)
-                elif isinstance(v, np.ndarray):
-                        d[k] = _check_arr(v)
-                else:
-                        pass
+    # If this is a 1x1 ndarray of matstructs, squeeze it.
+    if arr.size == 1:
+        a0 = arr.flatten()[0]
+        if isinstance(a0, sio.matlab.mio5_params.mat_struct):
+            return _check_dict_ff(_struct_to_dict(a0))
+
+    # we'll convert row vectors to 1-indexed dicts, with no entries for empty cells
+    if cellvec_to_dict and arr.shape[0] == 1:
+        d = {}
+        for idx, elem in enumerate(arr[0,:]):
+            key = idx+1
+            if isinstance(elem, sio.matlab.mio5_params.mat_struct):
+                d[key] = _check_dict_ff(_struct_to_dict(elem))
+            elif isinstance(elem, np.ndarray):
+                if elem.size == 0:
+                    continue
+                d[key] = _check_arr_ff(elem, cellvec_to_dict=True)
+            else:
+                warnings.warn("Array element is not a mat_struct, not an ndarray; type = %s" % type(elem))
+                d[key] = elem
         return d
 
-def _check_arr(ndarray):
-        '''
-        A recursive function which constructs lists from cellarrays
-        (which are loaded as numpy ndarrays), recursing into the elements
-        '''
-        # TODO: test datatype for 'O' and bail
-        if ndarray.dtype != np.object: return ndarray
-        
-        for index, elem in np.ndenumerate(ndarray):
-                if isinstance(elem, sio.matlab.mio5_params.mat_struct):
-                        ndarray[index] = _todict(elem)
-                elif isinstance(elem, np.ndarray):
-                        ndarray[index] = _check_arr(elem)
-                else:
-                        pass
-        return ndarray
+    # For cell arrays that are not row vectors, return them as ndarrays
+    for index, elem in np.ndenumerate(arr):
+        if isinstance(elem, sio.matlab.mio5_params.mat_struct):
+            arr[index] = _check_dict_ff(_struct_to_dict(elem))
+        elif isinstance(elem, np.ndarray):
+            arr[index] = _check_arr_ff(elem, cellvec_to_dict=False)
+        else:
+            warnings.warn("Array element is not a mat_struct, not an ndarray; type = %s" % type(elem))
+            arr[index] = elem
+    return arr
 
+def _struct_to_dict(matstruct):
+    d = matstruct.__dict__ # use dict provided by sio.loadmat
+    del d['_fieldnames'] # remove private housekeeping item from dict
+    return d
+
+    
 def get_files_by_day(base_dir, prefix, data_type):
         ''' Get files of a specific data types and return them in a dictionary indexed by day'''
         files = glob.glob("%s/%s%s*.mat" % (base_dir, prefix, data_type))
@@ -119,8 +141,8 @@ def build_day_eeg(files_by_tetrode, samprate):
         for file_info in files_by_tetrode:
                 print('loading file: %s' % file_info[0])
                 (day, epoch, tet_num) = file_info[1]
-                mat = loadmat2(file_info[0]) # use loadmat2 to avoid squeezing when day, epoch or tet=1
-                eeg = mat['eeg'][0,day-1][0,epoch-1][0,tet_num-1][0,0] # last index is for struct array
+                mat_eeg = loadmat_ff(file_info[0], 'eeg') # use loadmat2 to avoid squeezing when day, epoch or tet=1
+                eeg = mat_eeg[day][epoch][tet_num] # last index is for struct array
                 # create a list of times for the data
                 t = np.concatenate((t, eeg['starttime'] + (np.arange(0,len(eeg['data'])).reshape(-1,1) / samprate)))
                 d = np.concatenate((d, eeg['data']))
